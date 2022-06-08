858f534724e0668b78f9a4684b72eeaf487a7bd1
bokbokbokbok <57594308+bokbokbokbok@users.noreply.github.com>
2022-06-08 22:29:55 +0300

    Webview control add

--- TestWinui/MainWindow.xaml.cs
+++ TestWinui/MainWindow.xaml.cs
@@ -10,8 +10,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Popups;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -30,7 +33,41 @@ namespace TestWinui
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            PageRedirect(addressBar.Text);
+        }
+
+        private void OnKeyDownHandler(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                PageRedirect(addressBar.Text);
+            }
+        }
+
+        private void PageRedirect(string url)
+        {
+            try
+            {
+                Uri targetUri = new Uri(url);
+                MyWebView.Source = targetUri;
+            }
+            catch (FormatException ex)
+            {
+                ShowErrorPopup(url);
+            }
+        }
+
+        private async void ShowErrorPopup(string url)
+        {
+            ContentDialog noWifiDialog = new ContentDialog()
+            {
+                Title = "Incorrect url",
+                Content = url + " url was not in the correct format. Please enter a correct url.",
+                CloseButtonText = "Ok"
+            };
+
+            noWifiDialog.XamlRoot = HomeGrid.XamlRoot;
+            await noWifiDialog.ShowAsync();
         }
     }
 }
